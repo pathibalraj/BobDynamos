a330683aed7b6c89db183a64c603951647b409e3
Patti balaraj <pattibalraj@gmail.com>
2026-07-20 17:40:49 +0530

    test2 added

--- test.sql
+++ test.sql
@@ -1,1 +1,1 @@
-CREATE OR REPLACE TABLE BOB_TEST.BOB_SF.test(id number,name varchar,email varchar,Name_1 varchar,Name_2 varchar, address varchar,test varchar,test1 varchar);
+CREATE OR REPLACE TABLE BOB_TEST.BOB_SF.test(id number,name varchar,email varchar,Name_1 varchar,Name_2 varchar, address varchar,test varchar,test1 varchar,test2 varchar);
